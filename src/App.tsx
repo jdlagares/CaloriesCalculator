9c1915dffdcbe53404b7df6de9e42388c8cdbc95
jdlagares <sportymammal@gmail.com>
2026-08-12 14:44:11 -0500

    implement Reducer - add action save activity

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import Form from "./Components/Form"
+
+import { useReducer } from "react"
+import Form from "./components/Form"
+import { activityReducer,initialState } from "./reducers/activity-reducer"
+import ActivityList from "./components/ActivityList"
 
 function App() {
   
+  const [state,dispatch]= useReducer(activityReducer,initialState)
 
   return (
     <>
@@ -14,9 +19,16 @@ function App() {
       </header>
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form/>
+          <Form
+            dispatch={dispatch}
+          />
         </div>
 
+      </section>
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList
+          activities={state.activities}
+        />
       </section>
     </>
   )
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,15 +1,23 @@
 import { useState } from "react"
-import type {ChangeEvent,SubmitEvent } from "react"
+import type {ChangeEvent,Dispatch,SubmitEvent } from "react"
+import {v4 as uuidv4} from "uuid"
 import type { Activity } from "../types"
-import { categories } from "../data/catogories"
+import { categories } from "../data/categories"
+import type { ActivityActions } from "../reducers/activity-reducer"
 
-export default function Form() {
+type FormProps ={
+    dispatch: Dispatch<ActivityActions>
+}
 
-    const [activity,setActivity]=useState<Activity>({
-        category:1,
-        name:"",
-        calories:0
-    })
+const initialState:Activity={
+    id:uuidv4(),
+    category:1,
+    name:"",
+    calories:0
+}
+export default function Form({dispatch}:FormProps) {
+
+    const [activity,setActivity]=useState<Activity>(initialState)
 
     const handleChange =(e:ChangeEvent<HTMLInputElement | HTMLSelectElement>)=>{
         const IsNumberField=["category","calories"].includes(e.target.id)
@@ -25,7 +33,12 @@ export default function Form() {
     }
 
     const handleSubmit = (e:SubmitEvent<HTMLFormElement>)=>{
-
+        e.preventDefault()
+        dispatch({type:"save-activity", payload:{newActivity:activity}})
+        setActivity({
+            ...initialState,
+            id:uuidv4()
+        })
     }
   return (
     <form 
